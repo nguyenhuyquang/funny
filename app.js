53e0d2e01128506dbdfe8db96a73f74919d1a60e
quang <anhquang0611@gmail.com>
2021-12-17 18:34:42 +0700

    re mobile

--- app.js
+++ app.js
@@ -75,26 +75,60 @@ document.addEventListener('DOMContentLoaded', function(){
         document.querySelector('.swal2-styled').style.boxShadow = '0 0 0 3px rgba(0,0,0,.0)/50%';
     })
     document.querySelector('#play').click();
-    document.querySelector('.btn').addEventListener('click', function() {
-        document.querySelector('.left-container').style.paddingTop = '1%';
-        document.querySelector('.left-container').style.transition = 'padding-top 0.5s'
-        document.querySelector('.question').style.display = 'block';
-        document.querySelector('.answer').style.display = 'block';
-    })
-    document.querySelector('.btn-no').addEventListener('mouseover', function() {
-        document.querySelector('.btn-no').style.transition = 'opacity 0.2s ease-out';
-        document.querySelector('.btn-no').style.opacity = '0';
-    })
-    document.querySelector('.btn-no').addEventListener('mouseout', function() {
-        document.querySelector('.btn-no').style.transition = 'opacity 0.2s ease-in';
-        document.querySelector('.btn-no').style.opacity = '1';
-    })
-    document.querySelector('.btn-yes').addEventListener('click', function() {
-        document.querySelector('.btn').style.display = 'none';
-        document.querySelector('.left-container').style.paddingTop = '5%';
-        document.querySelector('.left-container').style.transition = 'padding-top 0.5s'
-        document.querySelector('.question').style.display = 'none';
-        document.querySelector('.answer').style.display = 'none';
-        document.querySelector('.thanks').style.display = 'block';
-    })
+   
+    var cW = document.documentElement.clientWidth; 
+    var cH = document.documentElement.clientHeight; 
+    console.log(cW + ' ' + cH);
+    if (cW < 500) {
+        document.querySelector('.btn').addEventListener('click', function() {
+            document.querySelector('.question').style.display = 'block';
+            document.querySelector('.question').style.width = '230px';
+            document.querySelector('.answer').style.display = 'flex';
+            document.querySelector('.answer').style.width = '230px';
+            document.querySelector('.answer').style.opacity = '1';
+            document.querySelector('.question').style.opacity = '1';
+            document.querySelector('.answer').style.transition = 'opacity 0.5s ease-in';
+            document.querySelector('.question').style.transition = 'opacity 0.2s ease-in';
+        })
+        document.querySelector('.btn-no').addEventListener('click', function() {
+            document.querySelector('.btn-no').style.position = 'fixed';
+            var random = Math.random();
+            var top = random*cH;
+            var left = random*cW;
+            top > cH - 100 ? cH - 100 : top;
+            left > cW - 100 ? cW - 100 : left;
+            console.log(''+ top + 'px' + ' ' + ''+ left + 'px');
+            document.querySelector('.btn-no').style.top = ''+ top + 'px';
+            document.querySelector('.btn-no').style.left = ''+ left + 'px';
+        })
+        document.querySelector('.btn-yes').addEventListener('click', function() {
+            document.querySelector('.btn').style.display = 'none';
+            document.querySelector('.question').style.display = 'none';
+            document.querySelector('.answer').style.display = 'none';
+            document.querySelector('.thanks').style.display = 'block';
+        })
+    } else {
+        document.querySelector('.btn').addEventListener('click', function() {
+            document.querySelector('.left-container').style.paddingTop = '1%';
+            document.querySelector('.left-container').style.transition = 'padding-top 0.5s'
+            document.querySelector('.question').style.display = 'block';
+            document.querySelector('.answer').style.display = 'block';
+        })
+        document.querySelector('.btn-no').addEventListener('mouseover', function() {
+            document.querySelector('.btn-no').style.transition = 'opacity 0.2s ease-out';
+            document.querySelector('.btn-no').style.opacity = '0';
+        })
+        document.querySelector('.btn-no').addEventListener('mouseout', function() {
+            document.querySelector('.btn-no').style.transition = 'opacity 0.2s ease-in';
+            document.querySelector('.btn-no').style.opacity = '1';
+        })
+        document.querySelector('.btn-yes').addEventListener('click', function() {
+            document.querySelector('.btn').style.display = 'none';
+            document.querySelector('.left-container').style.paddingTop = '5%';
+            document.querySelector('.left-container').style.transition = 'padding-top 0.5s'
+            document.querySelector('.question').style.display = 'none';
+            document.querySelector('.answer').style.display = 'none';
+            document.querySelector('.thanks').style.display = 'block';
+        })
+    }
 });
